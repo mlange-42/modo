b0574b14b821b7de1d20b9079817a579d2fb80bf
mlange-42 <martin_lange_@gmx.net>
2025-01-10 21:42:29 +0100

    define json types

---
+++ cmd/modo/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/mlange-24/modo"
+)
+
+func main() {
+	data, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal("Error reading stdin:", err)
+	}
+
+	docs, err := modo.FromJson(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_ = docs
+
+	fmt.Println("Success!")
+}
