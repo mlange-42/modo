83e12e9981591f19178903b9e4eba2ec421aa6c4
mlange-42 <martin_lange_@gmx.net>
2025-01-13 21:36:57 +0100

    render absolute and relative links

--- document/util.go
+++ document/util.go
@@ -12,8 +12,9 @@ import (
 const regexString = `(?s)(?:(` + "```.*?```)|(`.*?`" + `))|(\[.*?\])`
 
 type elemPath struct {
-	Elements []string
-	Kind     string
+	Elements  []string
+	Kind      string
+	IsSection bool
 }
 
 func findLinks(text string) ([]int, error) {
@@ -37,6 +38,9 @@ func findLinks(text string) ([]int, error) {
 
 func ProcessLinks(doc *Docs, t *template.Template) error {
 	lookup := collectPaths(doc)
+	//for k, v := range lookup {
+	//	fmt.Println(k, v.Elements)
+	//}
 	return processLinksPackage(doc.Decl, []string{}, lookup, t)
 }
 
@@ -44,17 +48,88 @@ func processLinksPackage(p *Package, elems []string, lookup map[string]elemPath,
 	newElems := appendNew(elems, p.GetName())
 
 	var err error
-	p.Summary, err = replaceLinks(p.Summary, newElems, lookup, t)
+	p.Summary, err = replaceLinks(p.Summary, newElems, len(newElems), lookup, t)
 	if err != nil {
 		return err
 	}
-	p.Description, err = replaceLinks(p.Summary, newElems, lookup, t)
+	p.Description, err = replaceLinks(p.Description, newElems, len(newElems), lookup, t)
 	if err != nil {
 		return err
 	}
 
 	for _, pkg := range p.Packages {
 		processLinksPackage(pkg, newElems, lookup, t)
+	}
+	for _, mod := range p.Modules {
+		processLinksModule(mod, newElems, lookup, t)
+	}
+
+	return nil
+}
+
+func processLinksModule(m *Module, elems []string, lookup map[string]elemPath, t *template.Template) error {
+	newElems := appendNew(elems, m.GetName())
+
+	var err error
+	m.Summary, err = replaceLinks(m.Summary, newElems, len(newElems), lookup, t)
+	if err != nil {
+		return err
+	}
+	m.Description, err = replaceLinks(m.Description, newElems, len(newElems), lookup, t)
+	if err != nil {
+		return err
+	}
+
+	for _, f := range m.Functions {
+		err := processLinksFunction(f, newElems, lookup, t)
+		if err != nil {
+			return err
+		}
+	}
+	for _, s := range m.Structs {
+		err := processLinksStruct(s, newElems, lookup, t)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func processLinksStruct(s *Struct, elems []string, lookup map[string]elemPath, t *template.Template) error {
+	newElems := appendNew(elems, s.GetName())
+
+	var err error
+	s.Summary, err = replaceLinks(s.Summary, newElems, len(elems), lookup, t)
+	if err != nil {
+		return err
+	}
+	s.Description, err = replaceLinks(s.Description, newElems, len(elems), lookup, t)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func processLinksFunction(f *Function, elems []string, lookup map[string]elemPath, t *template.Template) error {
+	newElems := appendNew(elems, f.GetName())
+
+	var err error
+	f.Summary, err = replaceLinks(f.Summary, newElems, len(elems), lookup, t)
+	if err != nil {
+		return err
+	}
+	f.Description, err = replaceLinks(f.Description, newElems, len(elems), lookup, t)
+	if err != nil {
+		return err
+	}
+
+	for _, o := range f.Overloads {
+		err := processLinksFunction(o, elems, lookup, t)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
@@ -69,7 +144,7 @@ func collectPaths(doc *Docs) map[string]elemPath {
 func collectPathsPackage(p *Package, elems []string, pathElem []string, out map[string]elemPath) {
 	newElems := appendNew(elems, p.GetName())
 	newPath := appendNew(pathElem, p.GetFileName())
-	out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "package"}
+	out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "package", IsSection: false}
 
 	for _, pkg := range p.Packages {
 		collectPathsPackage(pkg, newElems, newPath, out)
@@ -82,7 +157,7 @@ func collectPathsPackage(p *Package, elems []string, pathElem []string, out map[
 func collectPathsModule(m *Module, elems []string, pathElem []string, out map[string]elemPath) {
 	newElems := appendNew(elems, m.GetName())
 	newPath := appendNew(pathElem, m.GetFileName())
-	out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "module"}
+	out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "module", IsSection: false}
 
 	for _, s := range m.Structs {
 		collectPathsStruct(s, newElems, newPath, out)
@@ -91,52 +166,52 @@ func collectPathsModule(m *Module, elems []string, pathElem []string, out map[st
 		collectPathsTrait(t, newElems, newPath, out)
 	}
 	for _, f := range m.Functions {
-		newElems := appendNew(elems, f.GetName())
-		newPath := appendNew(pathElem, f.GetFileName())
-		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member"}
+		newElems := appendNew(newElems, f.GetName())
+		newPath := appendNew(newPath, f.GetFileName())
+		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member", IsSection: false}
 	}
 }
 
 func collectPathsStruct(s *Struct, elems []string, pathElem []string, out map[string]elemPath) {
 	newElems := appendNew(elems, s.GetName())
 	newPath := appendNew(pathElem, s.GetFileName())
-	out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member"}
+	out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member", IsSection: false}
 
 	for _, f := range s.Parameters {
 		newElems := appendNew(elems, f.GetName())
 		newPath := appendNew(pathElem, "#Parameters")
-		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member"}
+		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member", IsSection: true}
 	}
 	for _, f := range s.Fields {
 		newElems := appendNew(elems, f.GetName())
 		newPath := appendNew(pathElem, "#Fields")
-		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member"}
+		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member", IsSection: true}
 	}
 	for _, f := range s.Functions {
 		newElems := appendNew(elems, f.GetName())
 		newPath := appendNew(pathElem, "#"+f.GetName())
-		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member"}
+		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member", IsSection: true}
 	}
 }
 
 func collectPathsTrait(t *Trait, elems []string, pathElem []string, out map[string]elemPath) {
 	newElems := appendNew(elems, t.GetName())
 	newPath := appendNew(pathElem, t.GetFileName())
-	out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member"}
+	out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member", IsSection: false}
 
 	for _, f := range t.Fields {
 		newElems := appendNew(elems, f.GetName())
 		newPath := appendNew(pathElem, "#Fields")
-		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member"}
+		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member", IsSection: true}
 	}
 	for _, f := range t.Functions {
 		newElems := appendNew(elems, f.GetName())
 		newPath := appendNew(pathElem, "#"+f.GetName())
-		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member"}
+		out[strings.Join(newElems, ".")] = elemPath{Elements: newPath, Kind: "member", IsSection: true}
 	}
 }
 
-func replaceLinks(text string, elems []string, lookup map[string]elemPath, t *template.Template) (string, error) {
+func replaceLinks(text string, elems []string, modElems int, lookup map[string]elemPath, t *template.Template) (string, error) {
 	indices, err := findLinks(text)
 	if err != nil {
 		return "", err
@@ -148,50 +223,94 @@ func replaceLinks(text string, elems []string, lookup map[string]elemPath, t *te
 		start, end := indices[i], indices[i+1]
 		link := text[start+1 : end-1]
 
-		dots := 0
-		pathPrefix := []string{}
-		for strings.HasPrefix(link[dots:], ".") {
-			dots++
-			pathPrefix = append(pathPrefix, "..")
-		}
-		if dots > len(elems) {
-			log.Printf("Too many leading dots in cross ref: %s", link)
-			continue
-		}
-
-		var fullLink string
-		linkText := link[dots:]
-		subElems := elems[:len(elems)-dots]
-		if len(subElems) == 0 {
-			fullLink = link[dots:]
-		} else {
-			fullLink = strings.Join(subElems, ".") + "." + linkText
-		}
-		elemPath, ok := lookup[fullLink]
+		entry, linkText, parts, ok := toLink(link, elems, modElems, lookup)
 		if !ok {
-			log.Printf("Can't resolve cross ref: %s (%s)", link, fullLink)
 			continue
 		}
 
-		pathPrefixStr := path.Join(pathPrefix...)
-		pathStr := strings.Builder{}
-		if strings.HasPrefix(elemPath.Elements[len(elemPath.Elements)-1], "#") {
-			fullPath := path.Join(pathPrefixStr, path.Join(elemPath.Elements[len(subElems):len(elemPath.Elements)-1]...))
-			err := t.ExecuteTemplate(&pathStr, elemPath.Kind+"_path.md", fullPath)
-			if err != nil {
-				return "", err
-			}
-			pathStr.WriteString(elemPath.Elements[len(elemPath.Elements)-1])
+		var basePath string
+		if entry.IsSection {
+			basePath = path.Join(parts[:len(parts)-1]...)
 		} else {
-			fullPath := path.Join(pathPrefixStr, path.Join(elemPath.Elements[len(subElems):]...))
-			err := t.ExecuteTemplate(&pathStr, elemPath.Kind+"_path.md", fullPath)
-			if err != nil {
-				return "", err
-			}
+			basePath = path.Join(parts...)
+		}
+		pathStr := strings.Builder{}
+		err := t.ExecuteTemplate(&pathStr, entry.Kind+"_path.md", basePath)
+		if err != nil {
+			return "", err
+		}
+		if entry.IsSection {
+			pathStr.WriteString(parts[len(parts)-1])
 		}
 		text = fmt.Sprintf("%s[%s](%s)%s", text[:start], linkText, pathStr.String(), text[end:])
+		fmt.Println(link, "-->", pathStr.String())
 	}
 	return text, nil
+}
+
+func toLink(link string, elems []string, modElems int, lookup map[string]elemPath) (*elemPath, string, []string, bool) {
+	if strings.HasPrefix(link, ".") {
+		return toRelLink(link, elems, modElems, lookup)
+	}
+	return toAbsLink(link, elems, modElems, lookup)
+}
+
+func toRelLink(link string, elems []string, modElems int, lookup map[string]elemPath) (*elemPath, string, []string, bool) {
+	dots := 0
+	fullPath := []string{}
+	for strings.HasPrefix(link[dots:], ".") {
+		if dots > 0 {
+			fullPath = append(fullPath, "..")
+		}
+		dots++
+	}
+	if dots > modElems {
+		log.Printf("Too many leading dots in cross ref: %s", link)
+		return nil, "", nil, false
+	}
+	fullLink := link
+	linkText := link[dots:]
+	subElems := elems[:modElems-(dots-1)]
+
+	if len(subElems) == 0 {
+		fullLink = link[dots:]
+	} else {
+		fullLink = strings.Join(subElems, ".") + "." + linkText
+	}
+
+	elemPath, ok := lookup[fullLink]
+	if !ok {
+		log.Printf("Can't resolve cross ref: %s (%s)", link, fullLink)
+		return nil, "", nil, false
+	}
+
+	fullPath = append(fullPath, elemPath.Elements[len(subElems):]...)
+	return &elemPath, link[dots:], fullPath, true
+}
+
+func toAbsLink(link string, elems []string, modElems int, lookup map[string]elemPath) (*elemPath, string, []string, bool) {
+	elemPath, ok := lookup[link]
+	if !ok {
+		log.Printf("Can't resolve cross ref: %s", link)
+		return nil, "", nil, false
+	}
+	skip := 0
+	for range modElems {
+		if len(elemPath.Elements) <= skip {
+			break
+		}
+		if elemPath.Elements[skip] == elems[skip] {
+			skip++
+		} else {
+			break
+		}
+	}
+	fullPath := []string{}
+	for range modElems - skip {
+		fullPath = append(fullPath, "..")
+	}
+	fullPath = append(fullPath, elemPath.Elements[skip:]...)
+	return &elemPath, link, fullPath, true
 }
 
 func appendNew[T any](sl []T, elems ...T) []T {
--- document/util_test.go
+++ document/util_test.go
@@ -1,7 +1,6 @@
 package document
 
 import (
-	"fmt"
 	"path"
 	"testing"
 	"text/template"
@@ -26,21 +25,90 @@ func TestFindLinks(t *testing.T) {
 }
 
 func TestReplaceLinks(t *testing.T) {
-	text := "A [Struct], a [Struct.member], a [.Trait], a [q.func]. And a [Markdown](link)."
+	text := "A [.Struct], a [.Struct.member], a [..Trait], a [.q.func], abs [stdlib.Trait]. And a [Markdown](link)."
 	lookup := map[string]elemPath{
 		"stdlib.Trait":           {Elements: []string{"stdlib", "Trait"}, Kind: "member"},
 		"stdlib.p.Struct":        {Elements: []string{"stdlib", "p", "Struct"}, Kind: "member"},
-		"stdlib.p.Struct.member": {Elements: []string{"stdlib", "p", "Struct", "#member"}, Kind: "member"},
+		"stdlib.p.Struct.member": {Elements: []string{"stdlib", "p", "Struct", "#member"}, Kind: "member", IsSection: true},
 		"stdlib.p.q.func":        {Elements: []string{"stdlib", "p", "q", "func"}, Kind: "member"},
 	}
-	elems := []string{"stdlib", "p"}
+	elems := []string{"stdlib", "p", "Struct"}
 	templ := template.New("all").Funcs(template.FuncMap{"pathJoin": path.Join})
 	templ, err := templ.Parse(`{{define "member_path.md"}}{{.}}.md{{end}}`)
 	assert.Nil(t, err)
 
-	out, err := replaceLinks(text, elems, lookup, templ)
+	out, err := replaceLinks(text, elems, 2, lookup, templ)
 	assert.Nil(t, err)
 
-	fmt.Println(out)
-	assert.Equal(t, "A [Struct](Struct.md), a [Struct.member](Struct.md#member), a [Trait](../Trait.md), a [q.func](q/func.md). And a [Markdown](link).", out)
+	assert.Equal(t, "A [Struct](Struct.md), a [Struct.member](Struct.md#member), a [Trait](../Trait.md), a [q.func](q/func.md), abs [stdlib.Trait](../Trait.md). And a [Markdown](link).", out)
+}
+
+func TestToRelLink(t *testing.T) {
+	lookup := map[string]elemPath{
+		"stdlib.Trait":           {Elements: []string{"stdlib", "Trait"}, Kind: "member", IsSection: false},
+		"stdlib.p.Struct":        {Elements: []string{"stdlib", "p", "Struct"}, Kind: "member", IsSection: false},
+		"stdlib.p.Struct2":       {Elements: []string{"stdlib", "p", "Struct2"}, Kind: "member", IsSection: false},
+		"stdlib.q.Struct3":       {Elements: []string{"stdlib", "q", "Struct"}, Kind: "member", IsSection: false},
+		"stdlib.q.Struct4":       {Elements: []string{"stdlib", "q", "Struct2"}, Kind: "member", IsSection: false},
+		"stdlib.p.Struct.member": {Elements: []string{"stdlib", "p", "Struct", "#member"}, Kind: "member", IsSection: true},
+		"stdlib.p.q.func":        {Elements: []string{"stdlib", "p", "q", "func"}, Kind: "member", IsSection: false},
+	}
+	elems := []string{"stdlib", "p", "Struct"}
+
+	_, text, link, ok := toLink("..q.Struct3", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, "q.Struct3", text)
+	assert.Equal(t, []string{"..", "q", "Struct"}, link)
+
+	_, _, link, ok = toLink(".Struct2", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, []string{"Struct2"}, link)
+
+	_, _, link, ok = toLink(".Struct.member", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, []string{"Struct", "#member"}, link)
+
+	_, _, link, ok = toLink("..Trait", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, []string{"..", "Trait"}, link)
+
+	_, text, link, ok = toLink(".q.func", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, "q.func", text)
+	assert.Equal(t, []string{"q", "func"}, link)
+}
+
+func TestToAbsLink(t *testing.T) {
+	lookup := map[string]elemPath{
+		"stdlib.Trait":           {Elements: []string{"stdlib", "Trait"}, Kind: "member", IsSection: false},
+		"stdlib.p.Struct":        {Elements: []string{"stdlib", "p", "Struct"}, Kind: "member", IsSection: false},
+		"stdlib.p.Struct2":       {Elements: []string{"stdlib", "p", "Struct2"}, Kind: "member", IsSection: false},
+		"stdlib.q.Struct3":       {Elements: []string{"stdlib", "q", "Struct"}, Kind: "member", IsSection: false},
+		"stdlib.q.Struct4":       {Elements: []string{"stdlib", "q", "Struct2"}, Kind: "member", IsSection: false},
+		"stdlib.p.Struct.member": {Elements: []string{"stdlib", "p", "Struct", "#member"}, Kind: "member", IsSection: true},
+		"stdlib.p.q.func":        {Elements: []string{"stdlib", "p", "q", "func"}, Kind: "member", IsSection: false},
+	}
+	elems := []string{"stdlib", "p", "Struct"}
+
+	_, text, link, ok := toLink("stdlib.q.Struct3", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, "stdlib.q.Struct3", text)
+	assert.Equal(t, []string{"..", "q", "Struct"}, link)
+
+	_, _, link, ok = toLink("stdlib.p.Struct2", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, []string{"Struct2"}, link)
+
+	_, _, link, ok = toLink("stdlib.p.Struct.member", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, []string{"Struct", "#member"}, link)
+
+	_, _, link, ok = toLink("stdlib.Trait", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, []string{"..", "Trait"}, link)
+
+	_, text, link, ok = toLink("stdlib.p.q.func", elems, 2, lookup)
+	assert.True(t, ok)
+	assert.Equal(t, "stdlib.p.q.func", text)
+	assert.Equal(t, []string{"q", "func"}, link)
 }
