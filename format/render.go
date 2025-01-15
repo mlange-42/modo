7baa3ce7cb48aeba207e74d79e48bdbb5b920da8
mlange-42 <martin_lange_@gmx.net>
2025-01-15 20:33:12 +0100

    fix file generation from exports

--- format/render.go
+++ format/render.go
@@ -53,7 +53,7 @@ func renderElement(data interface {
 
 func renderPackage(p *document.Package, dir string, proc *document.Processor) error {
 	if proc.UseExports {
-		return renderPackageExports(p, dir, proc, &members{Members: []member{{Include: true}}})
+		return renderPackageExports(p, dir, proc, &member{Include: true})
 	} else {
 		return renderPackageNoExports(p, dir, proc)
 	}
@@ -68,7 +68,7 @@ type member struct {
 	RelPath []string
 }
 
-func renderPackageExports(p *document.Package, dir string, proc *document.Processor, parentMembers *members) error {
+func renderPackageExports(p *document.Package, dir string, proc *document.Processor, parentMembers *member) error {
 	selfIncluded, toCrawl := collectExportMembers(parentMembers)
 	for _, ex := range p.Exports {
 		var newMember member
@@ -101,16 +101,20 @@ func renderPackageExports(p *document.Package, dir string, proc *document.Proces
 
 	for _, pkg := range p.Packages {
 		if mems, ok := toCrawl[pkg.Name]; ok {
-			if err := renderPackageExports(pkg, pkgPath, proc, mems); err != nil {
-				return err
+			for _, m := range mems.Members {
+				if err := renderPackageExports(pkg, pkgPath, proc, &m); err != nil {
+					return err
+				}
 			}
 		}
 	}
 
 	for _, mod := range p.Modules {
 		if mems, ok := toCrawl[mod.Name]; ok {
-			if err := renderModuleExports(mod, pkgPath, proc, mems); err != nil {
-				return err
+			for _, m := range mems.Members {
+				if err := renderModuleExports(mod, pkgPath, proc, &m); err != nil {
+					return err
+				}
 			}
 		}
 	}
@@ -128,27 +132,27 @@ func renderPackageExports(p *document.Package, dir string, proc *document.Proces
 	return nil
 }
 
-func collectExportMembers(parentMembers *members) (bool, map[string]*members) {
-	selfIncluded := false
-	toCrawl := map[string]*members{}
-	for _, mem := range parentMembers.Members {
-		if mem.Include {
-			selfIncluded = true
-			continue
-		}
-		var newMember member
-		if len(mem.RelPath) == 1 {
-			newMember = member{Include: true}
-		} else {
-			newMember = member{RelPath: mem.RelPath[1:]}
-		}
-		if members, ok := toCrawl[mem.RelPath[0]]; ok {
-			members.Members = append(members.Members, newMember)
-			continue
-		}
-		toCrawl[mem.RelPath[0]] = &members{Members: []member{newMember}}
+func collectExportMembers(parentMember *member) (selfIncluded bool, toCrawl map[string]*members) {
+	selfIncluded = false
+	toCrawl = map[string]*members{}
+
+	if parentMember.Include {
+		selfIncluded = true
+		return
 	}
-	return selfIncluded, toCrawl
+	var newMember member
+	if len(parentMember.RelPath) == 1 {
+		newMember = member{Include: true}
+	} else {
+		newMember = member{RelPath: parentMember.RelPath[1:]}
+	}
+	if members, ok := toCrawl[parentMember.RelPath[0]]; ok {
+		members.Members = append(members.Members, newMember)
+		return
+	}
+	toCrawl[parentMember.RelPath[0]] = &members{Members: []member{newMember}}
+
+	return
 }
 
 func renderPackageNoExports(p *document.Package, dir string, proc *document.Processor) error {
@@ -186,42 +190,23 @@ func renderPackageNoExports(p *document.Package, dir string, proc *document.Proc
 	return nil
 }
 
-func renderModuleExports(mod *document.Module, dir string, proc *document.Processor, parentMembers *members) error {
-	selfIncluded, toCrawl := collectExportMembers(parentMembers)
-
-	var modPath, modFile string
-	if selfIncluded {
-		modPath = path.Join(dir, mod.GetFileName())
-		if err := mkDirs(modPath); err != nil {
-			return err
-		}
-		var err error
-		modFile, err = proc.Formatter.ToFilePath(modPath, "module")
-		if err != nil {
-			return err
-		}
-	} else {
-		modPath = dir
-	}
-
-	if err := renderListExports(mod.Structs, modPath, proc, toCrawl); err != nil {
-		return err
-	}
-	if err := renderListExports(mod.Traits, modPath, proc, toCrawl); err != nil {
-		return err
-	}
-	if err := renderListExports(mod.Functions, modPath, proc, toCrawl); err != nil {
-		return err
-	}
+func renderModuleExports(mod *document.Module, dir string, proc *document.Processor, parentMember *member) error {
+	selfIncluded, _ := collectExportMembers(parentMember)
 
 	if selfIncluded {
-		text, err := renderElement(mod, proc)
-		if err != nil {
-			return err
-		}
-		if err := os.WriteFile(modFile, []byte(text), 0666); err != nil {
-			return err
-		}
+		return renderModuleNoExports(mod, dir, proc)
+	}
+
+	modPath := dir
+
+	if err := renderList(mod.Structs, modPath, proc); err != nil {
+		return err
+	}
+	if err := renderList(mod.Traits, modPath, proc); err != nil {
+		return err
+	}
+	if err := renderList(mod.Functions, modPath, proc); err != nil {
+		return err
 	}
 
 	return nil
@@ -238,13 +223,13 @@ func renderModuleNoExports(mod *document.Module, dir string, proc *document.Proc
 		return err
 	}
 
-	if err := renderListNoExports(mod.Structs, modPath, proc); err != nil {
+	if err := renderList(mod.Structs, modPath, proc); err != nil {
 		return err
 	}
-	if err := renderListNoExports(mod.Traits, modPath, proc); err != nil {
+	if err := renderList(mod.Traits, modPath, proc); err != nil {
 		return err
 	}
-	if err := renderListNoExports(mod.Functions, modPath, proc); err != nil {
+	if err := renderList(mod.Functions, modPath, proc); err != nil {
 		return err
 	}
 
@@ -259,33 +244,7 @@ func renderModuleNoExports(mod *document.Module, dir string, proc *document.Proc
 	return nil
 }
 
-func renderListExports[T interface {
-	document.Named
-	document.Kinded
-}](list []T, dir string, proc *document.Processor, parentMembers map[string]*members) error {
-	for _, elem := range list {
-		if _, ok := parentMembers[elem.GetName()]; !ok {
-			continue
-		}
-		text, err := renderElement(elem, proc)
-		if err != nil {
-			return err
-		}
-		memberPath := path.Join(dir, elem.GetFileName())
-
-		memberFile, err := proc.Formatter.ToFilePath(memberPath, elem.GetKind())
-		if err != nil {
-			return err
-		}
-
-		if err := os.WriteFile(memberFile, []byte(text), 0666); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func renderListNoExports[T interface {
+func renderList[T interface {
 	document.Named
 	document.Kinded
 }](list []T, dir string, proc *document.Processor) error {
