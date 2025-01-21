a7d0e4535bd9287c54e07f03e3ab96a12be5cafe
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2025-01-21 11:40:48 +0100

    Improve code block attributes parser (#88)

--- document/doctest_test.go
+++ document/doctest_test.go
@@ -7,6 +7,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestParseBlockAttributes(t *testing.T) {
+	tests := []struct {
+		Text, Name              string
+		Hide, Global, Ok, Error bool
+	}{
+		{"```mojo",
+			"", false, false, false, false},
+		{"```mojo {doctest=\"test\" hide=true global=true}",
+			"test", true, true, true, false},
+		{"```mojo { doctest=\"test\" hide=true global=true }",
+			"test", true, true, true, false},
+		{"```mojo {doctest=\"test\"}",
+			"test", false, false, true, false},
+		{"```mojo {other=\"abc\" doctest=\"test\"}",
+			"test", false, false, true, false},
+		{"```mojo {.class1 doctest=\"test\" class2}",
+			"test", false, false, true, false},
+	}
+
+	for _, test := range tests {
+		name, hide, global, ok, err := parseBlockAttr(test.Text)
+		assert.Equal(t, name, test.Name, "Name %s", test.Text)
+		assert.Equal(t, hide, test.Hide, "Hide %s", test.Text)
+		assert.Equal(t, global, test.Global, "Global %s", test.Text)
+		assert.Equal(t, ok, test.Ok, "Ok %s", test.Text)
+		assert.Equal(t, err != nil, test.Error, "Err %s %s", test.Text, err)
+	}
+}
+
 func TestExtractDocTests(t *testing.T) {
 	text := "Docstring\n" +
 		"\n" +
