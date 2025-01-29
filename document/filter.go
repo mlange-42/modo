21e99d1c8c82a55ee96ca3d333450202c03a0ae8
mlange-42 <martin_lange_@gmx.net>
2025-01-29 02:15:04 +0100

    Fall back to rendering everything when no re-exports

--- document/filter.go
+++ document/filter.go
@@ -16,16 +16,24 @@ func (proc *Processor) filterPackages() error {
 		return err
 	}
 
+	if proc.Config.UseExports && !anyExports {
+		msg := "no package re-exports found. As 'exports' are enabled, there would be no output.\n" +
+			"       Add re-exports or run without 'exports' enabled"
+		if !proc.Config.Strict {
+			msg += "\n       Falling back to rendering everything."
+		}
+		if err := proc.warnOrError("%s", msg); err != nil {
+			return err
+		}
+		proc.Config.UseExports = false
+	}
+
 	if !proc.Config.UseExports {
 		proc.ExportDocs = &Docs{
 			Version: proc.Docs.Version,
 			Decl:    proc.Docs.Decl,
 		}
 		return nil
-	}
-
-	if !anyExports {
-		return fmt.Errorf("no package re-exports found. As 'exports' are enabled, there will be no output.\n       Add re-exports or run without 'exports' enabled")
 	}
 
 	proc.ExportDocs = &Docs{
