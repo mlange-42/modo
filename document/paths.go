b9282b555229cb96c2050e4c5207ec70eb8375fe
mlange-42 <martin_lange_@gmx.net>
2025-02-02 23:50:17 +0100

    establish mapping from paths to elements

--- document/paths.go
+++ document/paths.go
@@ -10,6 +10,8 @@ type elemPath struct {
 	IsSection bool
 }
 
+type addPathFunc = func(Named, []string, []string, string, bool)
+
 // Collects lookup for link target paths.
 // Runs on the re-structured package.
 func (proc *Processor) collectPaths() {
@@ -19,14 +21,14 @@ func (proc *Processor) collectPaths() {
 
 // Collects the paths of all (sub)-elements in the original structure.
 func (proc *Processor) collectElementPaths() {
-	proc.allPaths = map[string]bool{}
+	proc.allPaths = map[string]Named{}
 	proc.collectPathsPackage(proc.Docs.Decl, []string{}, []string{}, proc.addElementPath)
 }
 
-func (proc *Processor) collectPathsPackage(p *Package, elems []string, pathElem []string, add func([]string, []string, string, bool)) {
+func (proc *Processor) collectPathsPackage(p *Package, elems []string, pathElem []string, add addPathFunc) {
 	newElems := appendNew(elems, p.GetName())
 	newPath := appendNew(pathElem, p.GetFileName())
-	add(newElems, newPath, "package", false)
+	add(p, newElems, newPath, "package", false)
 
 	for _, pkg := range p.Packages {
 		proc.collectPathsPackage(pkg, newElems, newPath, add)
@@ -35,87 +37,87 @@ func (proc *Processor) collectPathsPackage(p *Package, elems []string, pathElem 
 		proc.collectPathsModule(mod, newElems, newPath, add)
 	}
 
-	for _, s := range p.Structs {
-		proc.collectPathsStruct(s, newElems, newPath, add)
+	for _, e := range p.Structs {
+		proc.collectPathsStruct(e, newElems, newPath, add)
 	}
-	for _, t := range p.Traits {
-		proc.collectPathsTrait(t, newElems, newPath, add)
+	for _, e := range p.Traits {
+		proc.collectPathsTrait(e, newElems, newPath, add)
 	}
-	for _, a := range p.Aliases {
-		newElems := appendNew(newElems, a.GetName())
+	for _, e := range p.Aliases {
+		newElems := appendNew(newElems, e.GetName())
 		newPath := appendNew(newPath, "#aliases")
-		add(newElems, newPath, "package", true) // kind=package for correct link paths
+		add(e, newElems, newPath, "package", true) // kind=package for correct link paths
 	}
-	for _, f := range p.Functions {
-		newElems := appendNew(newElems, f.GetName())
-		newPath := appendNew(newPath, f.GetFileName())
-		add(newElems, newPath, "function", false)
+	for _, e := range p.Functions {
+		newElems := appendNew(newElems, e.GetName())
+		newPath := appendNew(newPath, e.GetFileName())
+		add(e, newElems, newPath, "function", false)
 	}
 }
 
-func (proc *Processor) collectPathsModule(m *Module, elems []string, pathElem []string, add func([]string, []string, string, bool)) {
+func (proc *Processor) collectPathsModule(m *Module, elems []string, pathElem []string, add addPathFunc) {
 	newElems := appendNew(elems, m.GetName())
 	newPath := appendNew(pathElem, m.GetFileName())
-	add(newElems, newPath, "module", false)
+	add(m, newElems, newPath, "module", false)
 
-	for _, s := range m.Structs {
-		proc.collectPathsStruct(s, newElems, newPath, add)
+	for _, e := range m.Structs {
+		proc.collectPathsStruct(e, newElems, newPath, add)
 	}
-	for _, t := range m.Traits {
-		proc.collectPathsTrait(t, newElems, newPath, add)
+	for _, e := range m.Traits {
+		proc.collectPathsTrait(e, newElems, newPath, add)
 	}
-	for _, a := range m.Aliases {
-		newElems := appendNew(newElems, a.GetName())
+	for _, e := range m.Aliases {
+		newElems := appendNew(newElems, e.GetName())
 		newPath := appendNew(newPath, "#aliases")
-		add(newElems, newPath, "module", true) // kind=module for correct link paths
+		add(e, newElems, newPath, "module", true) // kind=module for correct link paths
 	}
-	for _, f := range m.Functions {
-		newElems := appendNew(newElems, f.GetName())
-		newPath := appendNew(newPath, f.GetFileName())
-		add(newElems, newPath, "function", false)
+	for _, e := range m.Functions {
+		newElems := appendNew(newElems, e.GetName())
+		newPath := appendNew(newPath, e.GetFileName())
+		add(e, newElems, newPath, "function", false)
 	}
 }
 
-func (proc *Processor) collectPathsStruct(s *Struct, elems []string, pathElem []string, add func([]string, []string, string, bool)) {
+func (proc *Processor) collectPathsStruct(s *Struct, elems []string, pathElem []string, add addPathFunc) {
 	newElems := appendNew(elems, s.GetName())
 	newPath := appendNew(pathElem, s.GetFileName())
-	add(newElems, newPath, "struct", false)
+	add(s, newElems, newPath, "struct", false)
 
-	for _, f := range s.Aliases {
-		newElems := appendNew(newElems, f.GetName())
+	for _, e := range s.Aliases {
+		newElems := appendNew(newElems, e.GetName())
 		newPath := appendNew(newPath, "#aliases")
-		add(newElems, newPath, "member", true)
+		add(e, newElems, newPath, "member", true)
 	}
-	for _, f := range s.Parameters {
-		newElems := appendNew(newElems, f.GetName())
+	for _, e := range s.Parameters {
+		newElems := appendNew(newElems, e.GetName())
 		newPath := appendNew(newPath, "#parameters")
-		add(newElems, newPath, "member", true)
+		add(e, newElems, newPath, "member", true)
 	}
-	for _, f := range s.Fields {
-		newElems := appendNew(newElems, f.GetName())
+	for _, e := range s.Fields {
+		newElems := appendNew(newElems, e.GetName())
 		newPath := appendNew(newPath, "#fields")
-		add(newElems, newPath, "member", true)
+		add(e, newElems, newPath, "member", true)
 	}
-	for _, f := range s.Functions {
-		newElems := appendNew(newElems, f.GetName())
-		newPath := appendNew(newPath, "#"+strings.ToLower(f.GetName()))
-		add(newElems, newPath, "member", true)
+	for _, e := range s.Functions {
+		newElems := appendNew(newElems, e.GetName())
+		newPath := appendNew(newPath, "#"+strings.ToLower(e.GetName()))
+		add(e, newElems, newPath, "member", true)
 	}
 }
 
-func (proc *Processor) collectPathsTrait(t *Trait, elems []string, pathElem []string, add func([]string, []string, string, bool)) {
+func (proc *Processor) collectPathsTrait(t *Trait, elems []string, pathElem []string, add addPathFunc) {
 	newElems := appendNew(elems, t.GetName())
 	newPath := appendNew(pathElem, t.GetFileName())
-	add(newElems, newPath, "trait", false)
+	add(t, newElems, newPath, "trait", false)
 
-	for _, f := range t.Fields {
-		newElems := appendNew(newElems, f.GetName())
+	for _, e := range t.Fields {
+		newElems := appendNew(newElems, e.GetName())
 		newPath := appendNew(newPath, "#fields")
-		add(newElems, newPath, "member", true)
+		add(e, newElems, newPath, "member", true)
 	}
-	for _, f := range t.Functions {
-		newElems := appendNew(newElems, f.GetName())
-		newPath := appendNew(newPath, "#"+strings.ToLower(f.GetName()))
-		add(newElems, newPath, "member", true)
+	for _, e := range t.Functions {
+		newElems := appendNew(newElems, e.GetName())
+		newPath := appendNew(newPath, "#"+strings.ToLower(e.GetName()))
+		add(e, newElems, newPath, "member", true)
 	}
 }
