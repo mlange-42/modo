8856c5515e877c423ef652a250a614f537edf75e
mlange-42 <martin_lange_@gmx.net>
2025-02-05 12:46:52 +0100

    generate and print version

---
+++ cmd/version.go
@@ -0,0 +1,3 @@
+package cmd
+
+const version = "v0.10.1-10-gb382da2"
---
+++ cmd/version/generate.go
@@ -0,0 +1,27 @@
+package main
+
+//go:generate go run .
+//go:generate go fmt ../version.go
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	cmd := exec.Command("git", "describe", "--tags")
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	version := strings.TrimSpace(string(out))
+
+	versionFile := "../version.go"
+	content := fmt.Sprintf("package cmd\n\nconst version = \"%s\"\n", version)
+	if err := os.WriteFile(versionFile, []byte(content), 0666); err != nil {
+		panic(err)
+	}
+}
