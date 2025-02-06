98d3dc552e10b79204add22585eb3a8861d8d609
mlange-42 <martin_lange_@gmx.net>
2025-02-06 15:06:54 +0100

    refine tests on init

--- internal/cmd/init_test.go
+++ internal/cmd/init_test.go
@@ -1,22 +1,30 @@
 package cmd
 
 import (
+	"io"
 	"os"
 	"path"
+	"strings"
 	"testing"
 
 	"github.com/mlange-42/modo/internal/util"
 	"github.com/stretchr/testify/assert"
 )
 
-func setupProject(t *testing.T, dir string) string {
-	err := util.MkDirs(path.Join(dir, "src"))
+func setupProject(t *testing.T, dir string, packages []string) string {
+	err := util.MkDirs(path.Join(dir, "src/test"))
 	assert.Nil(t, err)
-	err = util.MkDirs(path.Join(dir, "src", "test"))
+	err = util.MkDirs(path.Join(dir, "foo"))
+	assert.Nil(t, err)
+	err = util.MkDirs(path.Join(dir, "bar"))
 	assert.Nil(t, err)
 
-	err = os.WriteFile(path.Join(dir, "src", "test", "__init__.mojo"), []byte{}, 0644)
-	assert.Nil(t, err)
+	for _, p := range packages {
+		err := util.MkDirs(path.Join(dir, p))
+		assert.Nil(t, err)
+		err = os.WriteFile(path.Join(dir, p, "__init__.mojo"), []byte{}, 0644)
+		assert.Nil(t, err)
+	}
 
 	cwd, err := os.Getwd()
 	assert.Nil(t, err)
@@ -26,19 +34,43 @@ func setupProject(t *testing.T, dir string) string {
 	return cwd
 }
 
+func captureOutput(f func() error) (string, error) {
+	orig := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	err := f()
+	os.Stdout = orig
+	w.Close()
+	out, _ := io.ReadAll(r)
+	return string(out), err
+}
+
 func TestInitHugo(t *testing.T) {
-	dir := t.TempDir()
+	allPackages := [][]string{
+		{"src"},
+		{"package"},
+		{"package/src"},
+		{"src/package"},
+	}
 
-	cwd := setupProject(t, dir)
+	for _, packages := range allPackages {
+		dir := t.TempDir()
+		cwd := setupProject(t, dir, packages)
 
-	cmd, err := initCommand()
-	assert.Nil(t, err)
+		cmd, err := initCommand()
+		assert.Nil(t, err)
 
-	cmd.SetArgs([]string{"hugo"})
+		cmd.SetArgs([]string{"hugo"})
 
-	err = cmd.Execute()
-	assert.Nil(t, err)
+		output, err := captureOutput(func() error {
+			err = cmd.Execute()
+			return err
+		})
+		assert.Nil(t, err)
 
-	err = os.Chdir(cwd)
-	assert.Nil(t, err)
+		err = os.Chdir(cwd)
+		assert.Nil(t, err)
+
+		assert.Equal(t, 2, strings.Count(output, "WARNING"))
+	}
 }
